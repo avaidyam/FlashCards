bdd8615665fc2d7201cf79dc4541a2516b2e2816
Aditya Vaidyam <avaidyam@purdue.edu>
2017-09-05 12:25:11 -0400

    Initial Commit

---
+++ FlashCards/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FlashCards
+//
+//  Created by Aditya Vaidyam on 9/5/17.
+//  Copyright © 2017 avaidyam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
